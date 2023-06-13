e2040933a5390641b63bd5b327eca48365f9a8ed
Navaneeth Kotian <navaneeth.kotian@niveussolutions.com>
2023-06-13 10:44:16 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "./pages/signup/Signup";
 import Login from "./pages/login/Login";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home/Home";
 
 export default function App() {
   return (
     <div>
       <BrowserRouter>
+        <Navbar/>
         <Routes>
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home/>} />
         </Routes>
       </BrowserRouter>
     </div>
