792b47fe676c32c9aa0c80a69034f21702773334
Rakshith Kotian <rakshith.kotian@niveussolutions.com>
2023-07-27 12:02:45 +0530

    scroll behaviour change

--- src/pages/login/User.jsx
+++ src/pages/login/User.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from '../../components/Header'
 
 export default function User() {
   return (
-    <div>
-      <h1>User Logged In Successufully.... </h1>
-    </div>
+    <>
+      <Header/>
+        <h1 style={{margin : "70px 0 0 50px"}}> User Logged In Successufully.... </h1>
+    </>
   )
 }
