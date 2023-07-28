1833f665e48a0ab6cfd6a39fa926bae7a2540e3e
Rakshith Kotian <rakshith.kotian@niveussolutions.com>
2023-07-28 15:24:37 +0530

    Deleting constants

--- src/constants/apiConstant.js
+++
@@ -1,5 +0,0 @@
-export const BASE_URL = "https://food-delivery-6v3x.onrender.com";
-
-//apis
-export const SIGNUP = "/signup";
-export const LOGIN = "/login";
--- src/constants/textConstants.js
+++
@@ -1,1 +0,0 @@
-export const TEXT = "From swanky upscale restaurants to the cosiest hidden gems serving the most incredible food, We covers it all. Explore menus, and millions of restaurant photos and reviews from users just like you, to find your next great meal."
