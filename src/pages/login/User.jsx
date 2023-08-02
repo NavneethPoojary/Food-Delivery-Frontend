320c11951475d864ca4b2627fa8029d69c1c630c
Rakshith Kotian <rakshith.kotian@niveussolutions.com>
2023-08-02 17:37:54 +0530

    User page reciepe mapping started

--- src/pages/login/User.jsx
+++ src/pages/login/User.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import axios from "axios";
 
+
 export default function User() {
+
   const [recipeData, setRecipeData] = useState(null);
 
   useEffect(() => {
@@ -13,51 +15,23 @@ export default function User() {
       const { data: data } = await res.data;
       setRecipeData(data.recipes);
     }
-
     fetchPizza();
   }, []);
-
-  //   async function fetchPizza() {
-  //     try {
-  //       const res = await axios.get(
-  //         `https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza`
-  //       );
-  //     //   console.log(res.data.data)
-  //       const { data: data } = await res.data;
-  //         let allRecipes = []
-  //         allRecipes.push(data.recipes)
-  //       setRecipeData(allRecipes);
-  //       console.log(recipeData);
-  //     } catch (error) {
-  //       console.log(error.message);
-  //     }
-  //   }
-
-  //   const { image_url, publisher, title } = recipeData;
-
-  //   const { recipes } = recipeData;
 
   return (
     <>
       <Header />
-      {/* <div>
-              <img src={recipe.image_url} />
-              <h3>{recipe.title}</h3>
-              <p>{recipe.publisher}</p>
-            </div> */}
       <ul>
-        {recipeData && recipeData.map((e) => <span>{e.publisher}</span>)}
-        {/* {recipes?.map((rec) => {
-          <li className="flex" key={rec.id}>
-            <img src={rec.image_url} alt={rec.title} className="image" />
-            <div className="flex ">
-              <p className="font-medium">{rec.publisher}</p>
-              <div className="mt-auto flex items-center justify-between">
-                <button type="small">Add to cart</button>
-              </div>
+        {recipeData && recipeData.map((rec) => (
+          <li className="container" key={rec.id}>
+            <div className="container">
+              <img src={rec.image_url}  className="hero-img" alt="hero image"/>
+                <p className="column">{rec.publisher}</p>
+                <p className="column">{rec.title}</p>
+                <button className="btn-block">Add to cart</button>
             </div>
-          </li>;
-        })} */}
+          </li>
+      ))}
       </ul>
     </>
   );
