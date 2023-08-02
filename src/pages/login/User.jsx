7c85737db0fb3e045e86c60c4acbfaeb3be97b56
Navaneeth Kotian <navaneeth.kotian@niveussolutions.com>
2023-08-02 20:52:15 +0530

    showing limited data on ui

--- src/pages/login/User.jsx
+++ src/pages/login/User.jsx
@@ -2,9 +2,7 @@ import React, { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import axios from "axios";
 
-
 export default function User() {
-
   const [recipeData, setRecipeData] = useState(null);
 
   useEffect(() => {
@@ -13,7 +11,9 @@ export default function User() {
         `https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza`
       );
       const { data: data } = await res.data;
-      setRecipeData(data.recipes);
+      const pizza = data.recipes;
+      const modifiedPizza = pizza.splice(0, 20);
+      setRecipeData(modifiedPizza);
     }
     fetchPizza();
   }, []);
@@ -22,16 +22,21 @@ export default function User() {
     <>
       <Header />
       <ul>
-        {recipeData && recipeData.map((rec) => (
-          <li className="container" key={rec.id}>
-            <div className="container">
-              <img src={rec.image_url}  className="hero-img" alt="hero image"/>
+        {recipeData &&
+          recipeData.map((rec) => (
+            <li className="container" key={rec.id}>
+              <div className="container">
+                <img
+                  src={rec.image_url}
+                  className="hero-img"
+                  alt="hero image"
+                />
                 <p className="column">{rec.publisher}</p>
                 <p className="column">{rec.title}</p>
                 <button className="btn-block">Add to cart</button>
-            </div>
-          </li>
-      ))}
+              </div>
+            </li>
+          ))}
       </ul>
     </>
   );
